105c57fb22e06c4db59498ade2ca1510da9139b0
jasonkiptoo <kiptoojason@gmail.com>
2022-08-22 15:28:58 +0300

    Completed assignmnent

--- index.js
+++ index.js
@@ -1,1 +1,29 @@
 // Code your solution here
+function findMatching(driversList,driver){
+
+const foundDrivers = driversList.filter(drivers => drivers.toUpperCase() === driver.toUpperCase());
+
+  return foundDrivers
+
+}
+///function to return drivers names when provided letters match
+
+function fuzzyMatch(driversNames, findDriver){
+    // const drivers = ['Bobby', 'Sammy', 'Sally', 'Annette', 'Sarah', 'Bobby']
+
+let drivers=driversNames.filter(drivers=> drivers.startsWith(findDriver))
+
+    return drivers
+}
+
+
+function matchName(driversNames, findDriver){
+
+    const matchingDriver = driversNames.filter(driversNames => {
+        return driversNames.name === findDriver;
+    })
+    return matchingDriver;
+}
+
+
+
